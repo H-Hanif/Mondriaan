2b871f18e78e67b8a1d1c77cba920c8ede562cf9
H-Hanif <302732920@student.rocmondriaan.nl>
2023-12-11 12:52:50 +0100

    alles klaar
    alles klaar

---
+++ javascript/opdracht 3/main.js
@@ -0,0 +1,64 @@
+const div = document.querySelector(".box");
+const btn = document.querySelector(".run");
+const btnChange = document.querySelector(".change")
+let toggle = true;
+const btn2 = document.querySelector
+
+
+// btn.addEventListener("click", Function(){
+//     const element = document.querySelector(".box")
+//     element.classList.toggle("error")
+
+// });
+
+
+
+/*btn.addEventListener("click", function () {
+    if (toggle) {
+        div.classList.add("error");
+    } else {
+        div.classList.remove("error");
+    }
+    toggle = !toggle;
+});
+
+
+
+
+
+
+
+
+btn.addEventListener("click", function () {
+    div.classList.add("error");
+
+});
+
+btnChange.addEventListener("click", function () {
+    div.classList.remove("error");
+});
+*/
+
+btn.addEventListener("click" , function(){
+    if (animated) {
+        div.classList.add("blinker");
+    } else {
+        div.classList.remove("blinker");
+    }
+});
+
+[12:51] Daim Hussain
+const toggleButton = document.querySelector('.run');
+const box = document.querySelector('.box');
+ 
+toggleButton.addEventListener('click', function() {
+  box.classList.toggle('error');
+});
+ 
+const box2 = document.querySelector('.box2');
+const btn = document.querySelector('.animate')
+ 
+btn.addEventListener('click', function() {
+  box2.classList.toggle('blinker');
+  box2.classList.add('font')
+});
